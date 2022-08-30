2c5f66dcfd33da35e5fb024955c78037c44e85a2
fusheng <88888888>
2022-08-30 21:38:14 +0800

    日志调整

--- twint/run.py
+++ twint/run.py
@@ -94,8 +94,10 @@ class Twint:
                         self.feed, self.init = feed.parse_tweets(self.config, response)
                     except NoMoreTweetsException as e:
                         logme.debug(__name__ + ':Twint:Feed:' + str(e))
-                        print('[!] ' + str(e) + ' Scraping will stop now.')
-                        print('found {} deleted tweets in this search.'.format(len(self.config.deleted)))
+                        logme.debug('[!] ' + str(e) + ' Scraping will stop now.')
+                        logme.debug('found {} deleted tweets in this search.'.format(len(self.config.deleted)))
+                        # print('[!] ' + str(e) + ' Scraping will stop now.')
+                        # print('found {} deleted tweets in this search.'.format(len(self.config.deleted)))
                         break
                 break
             except TimeoutError as e:
